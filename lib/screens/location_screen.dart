8bc5f3d2e8cd7303683fd742f7a6a9b15e0439fb
[new] <[safsafi.zidane02@gmail.com]>
2025-12-29 03:26:23 +0100

    first commit

---
+++ lib/screens/location_screen.dart
@@ -0,0 +1,126 @@
+import 'package:clima_flutter/screens/city_screen.dart';
+import 'package:clima_flutter/services/weather.dart';
+import 'package:flutter/material.dart';
+import 'package:clima_flutter/utilities/constants.dart';
+class LocationScreen extends StatefulWidget {
+  final locationWeather;
+  const LocationScreen ({super.key,this.locationWeather});
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+
+  WeatherModel weather = WeatherModel();
+
+  int ?temperature;
+  String ?weatherIcon;
+  String ?cityName;
+  String ?weatherMessage;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(dynamic weatherData){
+    setState(() {
+      if (weatherData == null){
+        temperature = 0;
+        weatherIcon ='Error';
+        weatherMessage = 'Unable to get weather data';
+        cityName='';
+        return;
+      }
+
+        double temp = weatherData['main']['temp'];
+        temperature = temp.toInt();
+        weatherIcon = weather.getWeatherIcon(weatherData['weather'][0]['id']);
+        weatherMessage = weather.getMessage(temperature ?? 0);
+        cityName = weatherData['name'];
+
+    });
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/location_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withValues(alpha: (0.8 * 255)), BlendMode.dstATop),
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () async{
+                      var weatherData = await weather.getLocationWeather();
+                      updateUI(weatherData);
+                    },
+                    child: Icon(
+                      Icons.near_me,
+                      size: 50.0,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      var typedName = await Navigator.push(context, MaterialPageRoute(builder: (context){
+                        return CityScreen();
+                      }));
+                      if (typedName !=null){
+                          var weatherData = await weather.getCityWeather(typedName);
+                          updateUI(weatherData);
+
+                      }
+                    },
+                    child: Icon(
+                      Icons.location_city,
+                      size: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '$temperature°',
+                      style: kTempTextStyle,
+                    ),
+                    Text(
+                      weatherIcon ?? 'Error',
+                      style: kConditionTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Text(
+                  "$weatherMessage in $cityName!",
+                  textAlign: TextAlign.right,
+                  style: kMessageTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
